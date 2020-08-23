34f023b9d83894421d47038248f2719219e7e5a5
akumachan <akumachanit@gmail.com>
2020-08-23 22:10:37 +0900

    Update: Title

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1 id="header">体感寿命計算機</h1>
+    <h1 id="header">体感余命計算機</h1>
     <div id="description">
       寿命を80才として<a href="https://ja.wikipedia.org/wiki/%E3%82%B8%E3%83%A3%E3%83%8D%E3%83%BC%E3%81%AE%E6%B3%95%E5%89%87">ジャネーの法則</a>を用いて体感寿命を計算します。計算式が合っているかはわかりません。
     </div>
